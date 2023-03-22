f5bc77a8ed734055dd50cb44c33574d77e5dbd99
Deepak Kumar <deepjindl65@gmail.com>
2023-03-22 11:52:03 +0530

    MyRedis : PING command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,36 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+  public static void main(String[] args) {
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
-       ServerSocket serverSocket = null;
-       Socket clientSocket = null;
-       int port = 6379;
-       try {
-         serverSocket = new ServerSocket(port);
-         serverSocket.setReuseAddress(true);
-         // Wait for connection from client.
-         clientSocket = serverSocket.accept();
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
-           System.out.println("IOException: " + e.getMessage());
-         }
-       }
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      clientSocket = serverSocket.accept();
+      OutputStream output = clientSocket.getOutputStream();
+      PrintWriter writer = new PrintWriter(output, true);
+      writer.println("+PONG\r");
+      clientSocket.close();
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
   }
 }
