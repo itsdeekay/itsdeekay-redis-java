c48471e7e6a8cec88c374157506dc8497f5b9bb0
Deepak Kumar <deepjindl65@gmail.com>
2023-03-23 20:37:54 +0530

    MyRedis: Multiple PING commands in same connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,15 +15,22 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    int port = 6379;
+    int port = 6378;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      OutputStream output = clientSocket.getOutputStream();
-      PrintWriter writer = new PrintWriter(output, true);
-      writer.println("+PONG\r");
+      BufferedReader inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      BufferedWriter outputStrem = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      String line = inputStream.readLine();
+      while (line != null && !line.equals("end")) {
+        System.out.println("Line: "+line);
+        if (line.equalsIgnoreCase("ping")) {
+          outputStrem.write("+PONG\r\n");
+          outputStrem.flush();
+        }
+      }
       clientSocket.close();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
