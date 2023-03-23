17567b71d3ce3351a152cd22eb3548dc2234dd2d
Deepak Kumar <deepjindl65@gmail.com>
2023-03-23 21:05:08 +0530

    MyRedis : Concurrenct connections to redis for PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,14 +12,38 @@ public class Main {
     // when running tests.
     System.out.println("Logs from your program will appear here!");
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      while(true){
+        Socket clientSocket = serverSocket.accept();
+        Thread t = new Thread(new ClientHandler(clientSocket));
+        t.start();
+      }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (serverSocket != null) {
+          serverSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+  static class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket){
+      this.clientSocket = clientSocket;
+    }
+
+    public void run(){
+      try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
       BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
       String line;
       while ((line = reader.readLine()) != null) {
@@ -30,17 +52,18 @@ public class Main {
           writer.flush();
         }
       }
-      clientSocket.close();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
+      } finally {
+        try {
+          if (clientSocket != null) {
+            clientSocket.close();
+          }
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        }
       }
     }
-  }
 }
+}
+
